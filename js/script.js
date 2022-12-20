a159d1746c1b21c9611730b7c875ea7f2d053b0a
Mickael Fernandes <fmickael291@gmail.com>
2022-12-20 20:20:42 -0300

    telas menores e medias responsivas

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function sumirMain() {
+  let menuNavegacao = document.getElementById("menu-navegacao");
+  // let img1 = document.getElementById('menu-navegacao');
+  if (menuNavegacao.classList.contains("collapse navbar-collapse show")) {
+    alert("aaaa");
+  }
+}
