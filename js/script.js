d2613de08b6d4eb8f642167d33640cf1675004f1
Mickael Fernandes <fmickael291@gmail.com>
2022-12-21 17:32:33 -0300

    segundo container finalizado para desktop e dispositivos menores

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-function sumirMain() {
-  let menuNavegacao = document.getElementById("menu-navegacao");
-  // let img1 = document.getElementById('menu-navegacao');
-  if (menuNavegacao.classList.contains("collapse navbar-collapse show")) {
-    alert("aaaa");
-  }
-}
+
